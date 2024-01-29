7d4c6afa115c076346dbfca6d96a566d50661c4e
megaxd274 <kazemirova.l@gmail.com>
2024-01-30 02:52:05 +0300

    refactor: change cjs to esm

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,12 @@
+import { fork } from 'child_process';
+import { join } from 'path';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const script = fork(
+    join(process.cwd(), 'src', 'cp', 'files', 'script.js'),
+    args
+  );
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['text', 'txt2', 'CLOSE']);
