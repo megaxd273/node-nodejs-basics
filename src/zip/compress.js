7b2a0024c5cb9aeb1f0c1b1d0848ba31851feaad
megaxd274 <kazemirova.l@gmail.com>
2024-01-30 00:34:51 +0300

    feat: add zip solution

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,16 @@
+import { pipeline } from 'stream/promises';
+import { createReadStream, createWriteStream } from 'fs';
+import { join } from 'path';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+  await pipeline(
+    createReadStream(
+      join(process.cwd(), 'src', 'zip', 'files', 'fileToCompress.txt')
+    ),
+    createGzip(),
+    createWriteStream(join(process.cwd(), 'src', 'zip', 'files', 'archive.gz'))
+  );
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,15 @@
+import { createGunzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+import { createReadStream, createWriteStream } from 'fs';
+import { join } from 'path';
 const decompress = async () => {
-    // Write your code here 
+  await pipeline(
+    createReadStream(join(process.cwd(), 'src', 'zip', 'files', 'archive.gz')),
+    createGunzip(),
+    createWriteStream(
+      join(process.cwd(), 'src', 'zip', 'files', 'fileToCompress.txt')
+    )
+  );
 };
 
 await decompress();
