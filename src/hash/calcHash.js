532aa2f2b2599f7cd18a39e039c2da79cc40a7f1
megaxd274 <kazemirova.l@gmail.com>
2024-01-30 00:06:59 +0300

    feat: add hash and streams solutions

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { createHash } from 'crypto';
+import { pipeline } from 'stream/promises';
+import fs from 'node:fs';
+import { join } from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash('sha256');
+  await pipeline(
+    fs.createReadStream(
+      join(process.cwd(), 'src', 'hash', 'files', 'fileToCalculateHashFor.txt')
+    ),
+    hash.digest('hex'),
+    process.stdout
+  );
 };
 
 await calculateHash();
