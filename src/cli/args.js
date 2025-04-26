c68ca056050fac84ff73435ec30e99f6bceab138
megaxd274 <kazemirova.l@gmail.com>
2025-04-26 15:58:46 +0300

    feat: add hash and modules tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const map4ik = new Map();
+  for (let i = 0; i < args.length; i += 2) {
+    map4ik.set(args[i], args[i + 1]);
+  }
+  for (const [key, value] of map4ik) {
+    console.log(`${key} is ${value}`);
+  }
 };
 
 parseArgs();
--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { createReadStream } from "fs";
+import { createHash } from "crypto";
+import { stdout } from "process";
+import { pipeline } from "stream/promises";
+
+const dirname = import.meta.dirname;
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+  const dest = createReadStream(`${dirname}/files/fileToCalculateHashFor.txt`);
+
+  hash.setEncoding("hex");
+  await pipeline(dest, hash, stdout);
 };
 
 await calculateHash();
+await new Promise((resolve) => setTimeout(resolve, 3000));
